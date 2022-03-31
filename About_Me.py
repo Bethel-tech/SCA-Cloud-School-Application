1c9d028f5010b5c761ac3cb4d5abe6b7909c9485
Bethel-tech <osahonb1@gmail.com>
2022-03-31 21:03:54 +0100

    First commit

---
+++ About_Me.py
@@ -0,0 +1,6 @@
+title = 'a little detail about me'
+print(title.upper())
+
+properties = ["name: Bethel", "size: 10", "height: 5'", "hobby: Singing", "complexion: Brownish"]
+for item in properties:
+    print(item)
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo is for my assessment for the SCACloud School Bootcamp
